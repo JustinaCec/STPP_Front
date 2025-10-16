c7e6b35893727d365c40a467a582b44ad8663ca2
GhostyJusty <juscec@ktu.lt>
2025-10-16 15:18:23 +0300

    fix?

--- school-helpdesk-frontend/src/Pages/Login.jsx
+++ school-helpdesk-frontend/src/Pages/Login.jsx
@@ -33,7 +33,9 @@ export default function Login() {
                 localStorage.setItem("refreshToken", data.refreshToken);
 
                 // ✅ Redirect after short delay
-                setTimeout(() => navigate("/admin"), 1000);
+                setTimeout(() => {
+                    navigate("/admin");
+                }, 1000);
 
                 setEmail("");
                 setPassword("");
